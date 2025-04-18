0d23a051b6af484bb804448790d02386e2610dae
Mikiel Gica <gicam@rpi.edu>
2025-04-18 02:49:35 -0400

    Try to pass runner 2

--- solver/quadratureRules.hpp
+++ solver/quadratureRules.hpp
@@ -1,6 +1,8 @@
 #ifndef SOLVER_QUADRATURE_RULES
 #define SOLVER_QUADRATURE_RULES
 
+#include <cmath>
+
 struct TwoByTwoGaussLegendre {
     std::size_t const numQuad = 4;
     double const quadXi[4] = {1.0/std::sqrt(3), -1.0/std::sqrt(3), 1.0/std::sqrt(3), 1.0/std::sqrt(3)};
